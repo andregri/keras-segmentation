32eec0f8a45c891a981b3d95e17ec3e8922ae590
Andrea Grillo <andrea.grillo96@live.com>
2020-08-15 14:50:40 +0200

    fix model.py

--- model.py
+++ model.py
@@ -26,7 +26,8 @@ def build_vgg(weights_path, input_width=224, input_height=224):
 
     # Block2
     vgg.add(Conv2D(128, (3, 3), activation="relu", padding="same",
-                   name="block2_conv1", data_format=IMAGE_ORDERING))
+                   name="block2_conv1", data_format=IMAGE_ORDERING,
+                   input_shape=(input_width, input_height, 3)))
     vgg.add(Conv2D(128, (3, 3), activation="relu", padding="same",
                    name="block2_conv2", data_format=IMAGE_ORDERING))
     vgg.add(MaxPooling2D((2, 2), strides=(2, 2), name="block2_pool",
@@ -34,7 +35,8 @@ def build_vgg(weights_path, input_width=224, input_height=224):
 
     # Block3
     vgg.add(Conv2D(256, (3, 3), activation="relu", padding="same",
-                   name="block3_conv1", data_format=IMAGE_ORDERING))
+                   name="block3_conv1", data_format=IMAGE_ORDERING,
+                   input_shape=(input_width, input_height, 3)))
     vgg.add(Conv2D(256, (3, 3), activation="relu", padding="same",
                    name="block3_conv2", data_format=IMAGE_ORDERING))
     vgg.add(Conv2D(256, (3, 3), activation="relu", padding="same",
@@ -44,7 +46,8 @@ def build_vgg(weights_path, input_width=224, input_height=224):
 
     # Block4
     vgg.add(Conv2D(512, (3, 3), activation="relu", padding="same",
-                   name="block4_conv1", data_format=IMAGE_ORDERING))
+                   name="block4_conv1", data_format=IMAGE_ORDERING,
+                   input_shape=(input_width, input_height, 3)))
     vgg.add(Conv2D(512, (3, 3), activation="relu", padding="same",
                    name="block4_conv2", data_format=IMAGE_ORDERING))
     vgg.add(Conv2D(512, (3, 3), activation="relu", padding="same",
@@ -54,7 +57,8 @@ def build_vgg(weights_path, input_width=224, input_height=224):
 
     # Block5
     vgg.add(Conv2D(512, (3, 3), activation="relu", padding="same",
-                   name="block5_conv1", data_format=IMAGE_ORDERING))
+                   name="block5_conv1", data_format=IMAGE_ORDERING,
+                   input_shape=(input_width, input_height, 3)))
     vgg.add(Conv2D(512, (3, 3), activation="relu", padding="same",
                    name="block5_conv2", data_format=IMAGE_ORDERING))
     vgg.add(Conv2D(512, (3, 3), activation="relu", padding="same",
