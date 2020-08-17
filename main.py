6ac92ae2a00f5ba53712b328b0164ee9b00354e3
Andrea Grillo <andrea.grillo96@live.com>
2020-08-17 10:49:35 +0200

    put the dataset code in main to see if it works

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 
+import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 # from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping,
 # ReduceLROnPlateau, CSVLogger, TensorBoard
 
-from core.generator.dataset import Dataset
 from core.model.callbacks import Callbacks
+from core.generator.dataset_helper import load_image_train, load_image_test, parse_image
+from core.generator.generator_helper import make_pair
 
 from pathlib import Path
 
@@ -49,24 +51,61 @@ if __name__ == "__main__":
     for key in ["train", "val", "test"]:
         img_paths[key].sort(), label_paths[key].sort()
 
-    # Create datasets
-    train_ds = Dataset(
-        img_paths=img_paths["train"],
-        label_paths=label_paths["train"],
-        batch_size=32,
-        buffer_size=1000,
-        seed=42,
-        augment=True
-    )
+    data_pairs = {}
+    for key in ["train", "val", "test"]:
+        data_pairs[key] = make_pair(img_paths[key], label_paths[key])
 
-    val_ds = Dataset(
-        img_paths=img_paths["val"],
-        label_paths=label_paths["val"],
-        batch_size=16,
-        buffer_size=1000,
-        seed=42,
-        augment=False
-    )
+    print(f"""
+    TRAIN: {len(data_pairs['train'])}, {len(data_pairs['train'])}
+    VAL:   {len(data_pairs['val'])}, {len(data_pairs['val'])}
+    TEST:  {len(data_pairs['test'])}, {len(data_pairs['test'])}
+    """)
+
+    # Create datasets
+    BATCH_SIZE = 32
+    BUFFER_SIZE = 1000
+    SEED = 42
+
+    # TRAIN DATASET
+    # Create a dataset containing the filenames of the images for training
+    train_img_filenames = [fn.as_posix() for fn in img_paths["train"]]
+    train_img_filename_ds = tf.data.Dataset.from_tensor_slices(
+        train_img_filenames)
+
+    # Create a dataset containing the filenames of the gt images for training
+    train_label_filenames = [fn.as_posix() for fn in label_paths["train"]]
+    train_label_filename_ds = tf.data.Dataset.from_tensor_slices(
+        train_label_filenames)
+
+    # Map the parse function over the dataset
+    train_filename_ds = tf.data.Dataset.zip(
+        (train_img_filename_ds, train_label_filename_ds))
+    train_ds = train_filename_ds.shuffle(len(train_img_filenames), seed=SEED)
+    train_ds = train_ds.map(parse_image)
+    train_ds = train_ds.map(load_image_train, num_parallel_calls=4)
+    train_ds = train_ds.repeat()
+    train_ds = train_ds.batch(BATCH_SIZE)
+    train_ds = train_ds.prefetch(buffer_size=1000)
+
+    # VALIDATION DATASET
+    # Create a dataset containing the filenames of the images for VALIDATION
+    val_img_filenames = [fn.as_posix() for fn in img_paths["val"]]
+    val_img_filename_ds = tf.data.Dataset.from_tensor_slices(val_img_filenames)
+
+    # Create a dataset containing the filenames of the gt images for VALIDATION
+    val_label_filenames = [fn.as_posix() for fn in label_paths["val"]]
+    val_label_filename_ds = tf.data.Dataset.from_tensor_slices(
+        val_label_filenames)
+
+    # Map the parse function over the dataset
+    val_filename_ds = tf.data.Dataset.zip(
+        (val_img_filename_ds, val_label_filename_ds))
+    val_ds = val_filename_ds.shuffle(len(val_img_filenames), seed=SEED)
+    val_ds = val_ds.map(parse_image)
+    val_ds = val_ds.map(load_image_test, num_parallel_calls=4)
+    val_ds = val_ds.repeat()
+    val_ds = val_ds.batch(BATCH_SIZE)
+    val_ds = val_ds.prefetch(buffer_size=1000)
 
     print("[+] Creating the model...")
     # Creating the FCN8 model
@@ -84,28 +123,18 @@ if __name__ == "__main__":
 
     print("[+] Training...")
     # Training the model
-    cb = Callbacks("data/chkpt/top_weights.h5", "data/logs/", "data/trainig.csv")
+    cb = Callbacks("data/chkpt/top_weights.h5",
+                   "data/logs/", "data/trainig.csv")
+
+    EPOCHS = 1
+
+    STEPS_PER_EPOCH = len(data_pairs['train']) // BATCH_SIZE
+    VALIDATION_STEPS = len(data_pairs['val']) // BATCH_SIZE
 
     results = model.fit(
-        train_ds.ds,
-        steps_per_epoch=train_ds.steps,
+        train_ds,
+        steps_per_epoch=STEPS_PER_EPOCH,
         epochs=20,
-        validation_data=val_ds.ds,
-        validation_steps=val_ds.steps,
+        validation_data=val_ds,
+        validation_steps=VALIDATION_STEPS,
         callbacks=cb(checkpoint=True, tensorboard=True, csv=True))
-
-    print("[+] Evaluate the model...")
-    # Create the test generator and evaluate the model
-    test_ds = Dataset(
-        img_paths=img_paths["test"],
-        label_paths=label_paths["test"],
-        batch_size=16,
-        buffer_size=1000,
-        seed=42,
-        augment=False
-    )
-
-    res = model.evaluate(
-        test_ds.ds,
-        steps=test_ds.steps,
-        verbose=2)
