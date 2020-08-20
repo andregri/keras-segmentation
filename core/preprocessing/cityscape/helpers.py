5f0fedd08e9f25e678e58dceb8754372e1eb2f3e
Andrea Grillo <andrea.grillo96@live.com>
2020-08-20 14:34:46 +0200

    formatting code

--- core/preprocessing/cityscape/helpers.py
+++ core/preprocessing/cityscape/helpers.py
@@ -2,13 +2,14 @@ import numpy as np
 
 
 def color_gt_image(gt_img, id2color):
-    """Take a ground-truth image where each pixel's value corresponds to a class
-    ID (1-channel image) and return a RGB image (3-channels) where each class
-    has a different color. label2color maps from class to color.
+    """Take a ground-truth image where each pixel's value corresponds to a
+    class ID (1-channel image) and return a RGB image (3-channels) where each
+    class has a different color. label2color maps from class to color.
 
     Arguments:
     gt_img (numpy array): ground-truth 1-channel image where each pixel's value
         corresponds to a class ID. Shape=(width,height).
+
     id2color (dict): dictionary whose keys are class IDs and values are
         triplets of ints (RGB color).
 
@@ -23,6 +24,7 @@ def color_gt_image(gt_img, id2color):
 
     return color_img
 
+
 def IoU(true_label_mask, pred_label_mask, id2name):
     """Mean Intersection over Union = TP / (FN + TP + FP)
     Given the ground truth images and the predicted masks, compute the mIoU.
@@ -30,10 +32,13 @@ def IoU(true_label_mask, pred_label_mask, id2name):
     Arguments:
         true_label_mask (np array): (width, height, n_classes) ground-truth
             masks.
+
         pred_label_mask (np array): (width, height, n_classes) predicted masks.
-        id2name (dict): a dictionary whose keys are class IDs and values are the
-            class names.
+
+        id2name (dict): a dictionary whose keys are class IDs and values are
+            the class names.
     """
+
     IoUs = []
     for i in id2name.keys():
         TP = np.sum((true_label_mask == i) & (pred_label_mask == i))
@@ -41,7 +46,8 @@ def IoU(true_label_mask, pred_label_mask, id2name):
         FN = np.sum((true_label_mask == i) & (pred_label_mask != i))
         IoU = TP / float(FN + TP + FP)
         IoUs.append(IoU)
-        print(f"{id2name[i]:22}:\t#TP={TP:6},\t#FP={FP:6},\t#FN={FN:6},\tIoU={IoU:4.3f}")
+        print(f"{id2name[i]:22}:\t"
+              "#TP={TP:6},\t#FP={FP:6},\t#FN={FN:6},\tIoU={IoU:4.3f}")
 
     mIoU = np.mean(IoUs)
     print(f"mean IoU: {mIoU:4.3f}")
